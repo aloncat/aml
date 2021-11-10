eef9b0b025161c831f26f994d91521550b804edd
Dmitry Maslov <dmaslov81@gmail.com>
2021-11-10 21:08:53 +1000

    VS project files for core library, platform definitions and basic utilities

---
+++ core/pch.h
@@ -0,0 +1,26 @@
+﻿//∙AML
+// Copyright (C) 2016-2021 Dmitry Maslov
+// For conditions of distribution and use, see readme.txt
+
+#pragma once
+
+// NB: platform.h обязательно должен присутствовать в pch.h, так как этот заголовок необходим во многих .cpp файлах, но он
+// не подключается в них явно. Это связано с исповедуемым принципом не подключать в .cpp того, что уже подключается в pch.h и
+// заголовке той же единицы трансляции. В заголовочных файлах мы, напротив, всегда подключаем всё, что необходимо (на случай,
+// если этот заголовок будет использован, например, из другого проекта, где вообще может не быть precompiled headers)
+#include "platform.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+
+#include <algorithm>
+#include <atomic>
+#include <exception>
+#include <functional>
+#include <string>
+#include <string_view>
+#include <type_traits>
+#include <utility>
+#include <vector>
---
+++ core/prefix.cpp
@@ -0,0 +1,5 @@
+﻿//∙AML
+// Copyright (C) 2016-2021 Dmitry Maslov
+// For conditions of distribution and use, see readme.txt
+
+#include "pch.h"
