3cfeb57c96bd50add97447e9f8b46189a9b77e2c
Dmitry Maslov <dmaslov81@gmail.com>
2021-11-29 14:52:17 +1000

    Added *Array and *Toggle classes

---
+++ core/array.h
@@ -0,0 +1,248 @@
+﻿//∙AML
+// Copyright (C) 2017-2021 Dmitry Maslov
+// For conditions of distribution and use, see readme.txt
+
+#pragma once
+
+#include "platform.h"
+#include "util.h"
+
+#include <string.h>
+#include <type_traits>
+
+namespace util {
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//   DynamicArray
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Класс DynamicArray - простейший контейнер, представляет собой массив элементов одного
+// типа. Массив заданного размера выделяется в куче при создании объекта. Контейнер может
+// быть использован только для простых типов без инициализатора (POD-types)
+
+//--------------------------------------------------------------------------------------------------------------------------------
+template<class T>
+class DynamicArray final
+{
+	AML_NONCOPYABLE(DynamicArray)
+	static_assert(std::is_pod_v<T>, "Only POD-types are allowed");
+
+public:
+	// Параметр size задаёт желаемый размер контейнера (количество элементов). Если параметр
+	// data задан, то элементы контейнера будут проинициализированы значениями из массива data
+	explicit DynamicArray(size_t size, const T* data = nullptr)
+		: m_Items(size ? new Item[size] : nullptr)
+	{
+		if (data)
+			memcpy(m_Items, data, size * sizeof(T));
+	}
+
+	~DynamicArray() noexcept
+	{
+		if (m_Items)
+			delete[] m_Items;
+	}
+
+	operator T*() noexcept
+	{
+		return reinterpret_cast<T*>(m_Items);
+	}
+
+	operator const T*() const noexcept
+	{
+		return reinterpret_cast<T*>(m_Items);
+	}
+
+private:
+	using Item = uint8_t[sizeof(T)];
+
+	Item* m_Items;
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//   SmartArray
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Класс SmartArray - простой контейнер, представляет собой массив элементов одного типа. Его главное отличие от DynamicArray
+// в том, что этот контейнер имеет внутренний буфер. Если запрошенный размер (параметр size конструктора) меньше или равен
+// значению capacity (по умолчанию 80), то массив для элементов будет размещён во внутреннем буфере (на стеке). Иначе он
+// будет выделен в куче. Контейнер может быть использован только для простых типов без инициализатора (POD-types)
+
+//--------------------------------------------------------------------------------------------------------------------------------
+template<class T, size_t capacity = 80>
+class SmartArray final
+{
+	AML_NONCOPYABLE(SmartArray)
+	static_assert(capacity > 0, "Capacity must be greater than 0");
+	static_assert(std::is_pod_v<T>, "Only POD-types are allowed");
+
+public:
+	// Параметр size задаёт желаемый размер контейнера (количество элементов). Если параметр
+	// data задан, то элементы контейнера будут проинициализированы значениями из массива data
+	explicit SmartArray(size_t size, const T* data = nullptr)
+		: m_Items((size <= capacity) ? m_Buffer : new Item[size])
+	{
+		if (data)
+			memcpy(m_Items, data, size * sizeof(T));
+	}
+
+	~SmartArray() noexcept
+	{
+		if (m_Items != m_Buffer)
+			delete[] m_Items;
+	}
+
+	operator T*() noexcept
+	{
+		return reinterpret_cast<T*>(m_Items);
+	}
+
+	operator const T*() const noexcept
+	{
+		return reinterpret_cast<T*>(m_Items);
+	}
+
+private:
+	using Item = uint8_t[sizeof(T)];
+
+	Item* m_Items;
+	Item m_Buffer[capacity];
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//   FlexibleArray
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Класс FlexibleArray - это контейнер, представляющий собой массив элементов одного типа. От DynamicArray и
+// SmartArray этот контейнер отличает то, что он может быть проинициализирован пользовательским массивом заданного
+// размера. Также, он может менять свой размер в сторону увеличения: в этом случае новый массив большего размера
+// всегда выделяется в куче. Контейнер может быть использован только для простых типов без инициализатора
+
+//--------------------------------------------------------------------------------------------------------------------------------
+template<class T>
+class FlexibleArray final
+{
+	AML_NONCOPYABLE(FlexibleArray)
+	static_assert(std::is_pod_v<T>, "Only POD-types are allowed");
+
+public:
+	// Инициализирует контейнер размером size элементов. Если значение параметра
+	// size больше 0, то массив соответствующего размера сразу выделяется в куче
+	explicit FlexibleArray(size_t size = 0)
+		: m_Buffer(size ? new Item[size] : nullptr)
+		, m_Size(size)
+	{
+		m_Items = m_Buffer;
+	}
+
+	// Инициализирует контейнер пользовательским массивом userBuffer размером userSize
+	// элементов. Этот массив будет использован для хранения элементов контейнера
+	FlexibleArray(T* userBuffer, size_t userSize) noexcept
+		: m_Items(reinterpret_cast<Item*>(userBuffer))
+		, m_Buffer(nullptr)
+		, m_Size(userBuffer ? userSize : 0)
+	{
+	}
+
+	template<size_t userSize>
+	explicit FlexibleArray(T (&userBuffer)[userSize]) noexcept
+		: m_Items(reinterpret_cast<Item*>(userBuffer))
+		, m_Buffer(nullptr)
+		, m_Size(userBuffer ? userSize : 0)
+	{
+	}
+
+	~FlexibleArray() noexcept
+	{
+		if (m_Buffer)
+			delete[] m_Buffer;
+	}
+
+	// Реинициализирует контейнер новым пользовательским
+	// массивом userBuffer размером userSize элементов
+	void Set(T* userBuffer, size_t userSize) noexcept
+	{
+		if (m_Buffer)
+			delete[] m_Buffer;
+		m_Items = reinterpret_cast<Item*>(userBuffer);
+		m_Buffer = nullptr;
+		m_Size = userBuffer ? userSize : 0;
+	}
+
+	template<size_t userSize>
+	void Set(T (&userBuffer)[userSize]) noexcept
+	{
+		Set(userBuffer, userSize);
+	}
+
+	size_t GetSize() const noexcept
+	{
+		return m_Size;
+	}
+
+	// Увеличивает (при необходимости) размер контейнера до newSize элементов. Если значение newSize
+	// меньше или равно текущему размеру контейнера, то ничего не меняется. Если retainContents
+	// равен true, то при увеличении размера контейнера его содержимое будет сохранено
+	void Grow(size_t newSize, bool retainContents = false)
+	{
+		if (newSize > m_Size)
+		{
+			if (retainContents)
+				Resize(newSize);
+			else
+				Reallocate(newSize);
+		}
+	}
+
+	operator T*() noexcept
+	{
+		return reinterpret_cast<T*>(m_Items);
+	}
+
+	operator const T*() const noexcept
+	{
+		return reinterpret_cast<T*>(m_Items);
+	}
+
+private:
+	using Item = uint8_t[sizeof(T)];
+
+	AML_NOINLINE void Reallocate(size_t newSize)
+	{
+		if (m_Buffer)
+			delete[] m_Buffer;
+		m_Items = m_Buffer = nullptr;
+		m_Size = 0;
+
+		m_Buffer = new Item[newSize];
+		m_Items = m_Buffer;
+		m_Size = newSize;
+	}
+
+	AML_NOINLINE void Resize(size_t newSize)
+	{
+		Item* newBuffer = new Item[newSize];
+		if (m_Size)
+		{
+			memcpy(newBuffer, m_Items, m_Size * sizeof(T));
+			if (m_Buffer)
+				delete[] m_Buffer;
+		}
+		m_Buffer = newBuffer;
+		m_Items = m_Buffer;
+		m_Size = newSize;
+	}
+
+private:
+	Item* m_Items;
+	Item* m_Buffer;
+	size_t m_Size;
+};
+
+} // namespace util
---
+++ core/toggle.h
@@ -0,0 +1,163 @@
+﻿//∙AML
+// Copyright (C) 2017-2021 Dmitry Maslov
+// For conditions of distribution and use, see readme.txt
+
+#pragma once
+
+#include "util.h"
+
+#include <functional>
+#include <type_traits>
+#include <utility>
+
+namespace util {
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//   Toggle
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Класс Toggle - это "переключатель". В момент своей инициализации он запоминает значение указанной
+// переменной, затем (опционально) присваивает ей новое значение. При разрушении объекта Toggle (при
+// выходе переменной из области видимости) исходное значение указанной переменной восстанавливается
+
+//--------------------------------------------------------------------------------------------------------------------------------
+template<class T>
+class Toggle
+{
+	AML_NONCOPYABLE(Toggle)
+	static_assert(std::is_nothrow_destructible_v<T> && (std::is_nothrow_move_assignable_v<T> ||
+		(!std::is_move_assignable_v<T> && std::is_nothrow_copy_assignable_v<T>)),
+		"Class T must have nothrow destructor & move/copy assignment operator");
+
+public:
+	// Инициализирует объект, не меняя значение переменной var
+	explicit Toggle(T& var)
+		: m_VarPtr(&var)
+		, m_Value(var)
+	{
+	}
+
+	// Инициализирует объект и меняет значение переменной var на newValue
+	Toggle(T& var, const T& newValue)
+		: m_VarPtr(&var)
+		, m_Value(std::move(var))
+	{
+		var = newValue;
+	}
+
+	// Инициализирует объект и меняет значение переменной var на newValue
+	Toggle(T& var, T&& newValue)
+		: m_VarPtr(&var)
+		, m_Value(std::move(var))
+	{
+		var = std::forward<T>(newValue);
+	}
+
+	// Восстанавливает исходное значение переменной
+	~Toggle() noexcept
+	{
+		// NB: для корректной работы класс T должен иметь nothrow оператор перемещающего
+		// присваивания или nothrow оператор копирования (только если перемещающего нет)
+		*m_VarPtr = std::move(m_Value);
+	}
+
+	// Возвращает исходное значение переменной
+	const T& GetOriginal() const noexcept
+	{
+		return m_Value;
+	}
+
+	// Задаёт новое "исходное" значение для переменной. Теперь при разрушении объекта
+	// переменной будет присвоенно значение newRestoreValue. Если параметр setNow равен
+	// true, то значение самой переменной также будет изменено на newRestoreValue
+	void SetRestore(const T& newRestoreValue, bool setNow = false)
+	{
+		m_Value = newRestoreValue;
+		if (setNow)
+			*m_VarPtr = m_Value;
+	}
+
+	// Функция аналогична предыдущей (но использует R-value)
+	void SetRestore(T&& newRestoreValue, bool setNow = false)
+	{
+		m_Value = std::forward<T>(newRestoreValue);
+		if (setNow)
+			*m_VarPtr = m_Value;
+	}
+
+	// Восстанавливает исходное значение переменной
+	void Restore()
+	{
+		*m_VarPtr = m_Value;
+	}
+
+	// Возвращает ссылку на переменную
+	T& operator *() noexcept { return *m_VarPtr; }
+	const T& operator *() const noexcept { return *m_VarPtr; }
+
+	// Позволяет обратиться к члену переменной (если переменная - это класс или структура)
+	T* operator ->() noexcept { return m_VarPtr; }
+	const T* operator ->() const noexcept { return m_VarPtr; }
+
+protected:
+	T* m_VarPtr;
+	T m_Value;
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//   FuncToggle
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Класс FuncToggle - это "переключатель" с пользовательской функцией. В момент своей
+// инициализации и разрушения он вызывает заданный (при вызове конструктора) функтор
+
+//--------------------------------------------------------------------------------------------------------------------------------
+class FuncToggle
+{
+	AML_NONCOPYABLE(FuncToggle)
+
+public:
+	// Прототип функции, которая будет вызываться конструктором и деструктором. При вызове функции из конструктора
+	// bool параметр будет равен true, а при вызове из деструктора - false. Если функция вернёт true в первый раз
+	// (при вызове из конструктора), она будет вызвана из деструктора, если вернёт false, то не будет
+	using ToggleFn = std::function<bool(bool)>;
+
+	FuncToggle(const ToggleFn& fn)
+		: m_Fn(fn)
+	{
+		if (m_Fn && !m_Fn(true))
+			m_Fn = nullptr;
+	}
+
+	FuncToggle(ToggleFn&& fn)
+		: m_Fn(std::move(fn))
+	{
+		if (m_Fn && !m_Fn(true))
+			m_Fn = nullptr;
+	}
+
+	~FuncToggle() noexcept(false)
+	{
+		if (m_Fn)
+			m_Fn(false);
+	}
+
+	// Вызывает пользовательскую функцию с параметром bool, равным false (так же, как это делает деструктор
+	// класса). Если функция вернула false или параметр noMoreRestore был равен true, то пользовательская
+	// функция не будет вызвана из деструктора при разрушении объекта. Если пользовательская функция
+	// вернула false в первый раз (при вызове из конструктора), то Restore её не вызовет
+	void Restore(bool noMoreRestore = false)
+	{
+		if (m_Fn && (!m_Fn(false) || noMoreRestore))
+			m_Fn = nullptr;
+	}
+
+protected:
+	ToggleFn m_Fn;
+};
+
+} // namespace util
