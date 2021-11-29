82df57dcaac3cb2bbed127427bd5bf4d6bd762cc
Dmitry Maslov <dmaslov81@gmail.com>
2021-11-29 13:13:38 +1000

    Added RandGen class, WinAPI helper and basic thread functions

---
+++ core/randgen.h
@@ -0,0 +1,39 @@
+﻿//∙AML
+// Copyright (C) 2017-2021 Dmitry Maslov
+// For conditions of distribution and use, see readme.txt
+
+#pragma once
+
+#include "platform.h"
+
+namespace math {
+
+//--------------------------------------------------------------------------------------------------------------------------------
+class RandGen
+{
+public:
+	RandGen();
+	explicit RandGen(unsigned seed) noexcept;
+
+	// Задаёт новое зерно генератора
+	void Seed(unsigned seed) noexcept;
+
+	// Генерирует псевдо-случайное 32-битное целое число
+	unsigned UInt() noexcept { return Next(); }
+	// Генерирует псевдо-случайное целое число X, такое что 0 <= X < range
+	unsigned UInt(unsigned range) noexcept;
+	// Генерирует псевдо-случайное целое число X, такое что min <= X <= max
+	unsigned UInt(unsigned min, unsigned max) noexcept;
+
+	// Генерирует псевдо-случайное дробное число X, такое что 0.0 <= X <= 1.0
+	float Float() noexcept;
+	// Генерирует псевдо-случайное дробное число X, такое что min <= X <= max
+	float Float(float min, float max) noexcept;
+
+protected:
+	uint32_t Next() noexcept;
+
+	uint32_t m_X, m_Y, m_Z;
+};
+
+} // namespace math
---
+++ core/thread.cpp
@@ -0,0 +1,51 @@
+﻿//∙AML
+// Copyright (C) 2017-2021 Dmitry Maslov
+// For conditions of distribution and use, see readme.txt
+
+#include "pch.h"
+#include "thread.h"
+
+#include "winapi.h"
+
+#if AML_OS_WINDOWS
+	#include <intrin.h>
+#endif
+
+namespace thread {
+
+//--------------------------------------------------------------------------------------------------------------------------------
+unsigned GetThreadId()
+{
+	#if AML_OS_WINDOWS
+		return ::GetCurrentThreadId();
+	#else
+		#error Not implemented
+	#endif
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+void CPUPause()
+{
+	#if AML_OS_WINDOWS
+		// Инструкция pause относится к набору SSE2, который впервые появился в Pentium 4. Поддержка SSE2
+		// требуется для установки Windows 8 и более новых ОС компании Microsoft. Также SSE2 присутствует
+		// на всех 64-битных процессорах Intel и AMD. Но несмотря на то, что инструкция pause появилась в
+		// наборе SSE2, на процессорах, не поддерживающих SSE2, она эквивалентна обычной инструкции nop.
+		// Т.о. использование этой инструкции не требует обязательной поддержки SSE2 процессором
+		_mm_pause();
+	#else
+		#error Not implemented
+	#endif
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+void Sleep(unsigned milliseconds)
+{
+	#if AML_OS_WINDOWS
+		::Sleep(milliseconds);
+	#else
+		#error Not implemented
+	#endif
+}
+
+} // namespace thread
