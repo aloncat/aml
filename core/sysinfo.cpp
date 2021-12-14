25f0f60f7941d7a9b737a7f449bf052e0c3bf44e
Dmitry Maslov <dmaslov81@gmail.com>
2021-12-14 18:11:01 +1000

    Added SystemInfo singleton

---
+++ core/sysinfo.cpp
@@ -0,0 +1,175 @@
+﻿//∙AML
+// Copyright (C) 2017-2021 Dmitry Maslov
+// For conditions of distribution and use, see readme.txt
+
+#include "pch.h"
+#include "sysinfo.h"
+
+#include "datetime.h"
+#include "filesystem.h"
+#include "winapi.h"
+
+#if AML_OS_WINDOWS
+	#include <shlobj.h>
+#endif
+
+using namespace util;
+
+//--------------------------------------------------------------------------------------------------------------------------------
+SystemInfo::SystemInfo()
+{
+	m_FirstTick = GetOSUptime();
+	m_LaunchDateTime = DateTime::Now(true);
+
+	InitCoreCount();
+
+	#if AML_OS_WINDOWS
+		const wchar_t* cmdLine = ::GetCommandLineW();
+		if (GetNextCmdLineParam(m_AppExePath, cmdLine))
+		{
+			if (!m_AppExePath.empty())
+			{
+				if (auto fullPath = FileSystem::GetFullPath(m_AppExePath); !fullPath.empty())
+					m_AppExePath = std::move(fullPath);
+			}
+			std::wstring param;
+			while (GetNextCmdLineParam(param, cmdLine))
+				m_CmdLineParameters.push_back(param);
+		}
+
+		wchar_t path[MAX_PATH];
+		if (::SHGetFolderPathW(nullptr, CSIDL_LOCAL_APPDATA, nullptr, SHGFP_TYPE_CURRENT, path) == S_OK)
+		{
+			std::wstring fullPath(path);
+			m_AppDataPath = FileSystem::GetFullPath(fullPath.append(L"\\"));
+		}
+	#else
+		#error Not implemented
+	#endif
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+unsigned SystemInfo::GetAppUptime() const
+{
+	return GetOSUptime() - m_FirstTick;
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+AML_NOINLINE unsigned SystemInfo::GetOSUptime()
+{
+	#if AML_OS_WINDOWS
+		uint64_t osUptime;
+		if (WinAPI::CanGetTickCount64())
+			osUptime = WinAPI::GetTickCount64();
+		else
+		{
+			size_t last = ::GetTickCount();
+			static const size_t first = last;
+			osUptime = last;
+			if (last < first)
+				osUptime += 1ll << 32;
+		}
+
+		return static_cast<unsigned>(osUptime / 1000);
+	#else
+		#error Not implemented
+	#endif
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+char SystemInfo::GetDecimalPoint(bool localeChanged) const
+{
+	static char cachedValue;
+	if (!cachedValue || localeChanged)
+	{
+		char buffer[8];
+		// Если локаль программы была установлена, то функции форматирования будут использовать в качестве
+		// десятичной точки символ, заданный в настройках ОС (по умолчанию в "C" локали используется точка)
+		auto i = sprintf_s(buffer, CountOf(buffer), "%.1f", .1f);
+		cachedValue = (i > 0) ? buffer[1] : '.';
+	}
+	return cachedValue;
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+bool SystemInfo::IsConsoleApp()
+{
+	#if AML_OS_WINDOWS
+		return ::GetConsoleWindow() != nullptr;
+	#else
+		#error Not implemented
+	#endif
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+void SystemInfo::InitCoreCount()
+{
+	#ifdef AML_OS_WINDOWS
+		SYSTEM_INFO sysInfo;
+		::GetSystemInfo(&sysInfo);
+		m_CoreCount.logical = sysInfo.dwNumberOfProcessors ? sysInfo.dwNumberOfProcessors : 1;
+		m_CoreCount.physical = m_CoreCount.logical;
+
+		DWORD bufferSize = 0;
+		::GetLogicalProcessorInformation(nullptr, &bufferSize);
+		if (::GetLastError() == ERROR_INSUFFICIENT_BUFFER && bufferSize)
+		{
+			void* p = new uint8_t[bufferSize];
+			auto info = static_cast<PSYSTEM_LOGICAL_PROCESSOR_INFORMATION>(p);
+			if (::GetLogicalProcessorInformation(info, &bufferSize))
+			{
+				unsigned coreC = 0, logicalC = 0;
+				for (size_t size = 0; size < bufferSize; size += sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION))
+				{
+					if (info->Relationship == RelationProcessorCore)
+					{
+						++coreC;
+						for (auto mask = info->ProcessorMask; mask; mask >>= 1)
+							logicalC += (mask & 1) ? 1 : 0;
+					}
+					++info;
+				}
+				m_CoreCount.logical = logicalC ? logicalC : 1;
+				m_CoreCount.physical = coreC ? coreC : 1;
+				if (m_CoreCount.logical < m_CoreCount.physical)
+					m_CoreCount.logical = m_CoreCount.physical;
+			}
+			delete[] p;
+		}
+	#else
+		#error Not implemented
+	#endif
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+bool SystemInfo::GetNextCmdLineParam(std::wstring& out, const wchar_t*& cmdLine)
+{
+	out.clear();
+	if (!cmdLine || !cmdLine[0])
+		return false;
+
+	const wchar_t* p = cmdLine;
+	while (*p == 32) ++p;
+
+	bool allowEmpty = false;
+	const wchar_t* from = p;
+	for (bool quote = false; *p; ++p)
+	{
+		if (*p == '"')
+		{
+			quote = !quote;
+			allowEmpty = true;
+			if (p > from)
+				out.append(from, p - from);
+			from = p + 1;
+		}
+		else if (*p == 32 && !quote)
+			break;
+	}
+
+	cmdLine = p;
+	if (p > from)
+		out.append(from, p - from);
+
+	return !out.empty() || allowEmpty;
+}
---
+++ core/sysinfo.h
@@ -0,0 +1,77 @@
+﻿//∙AML
+// Copyright (C) 2017-2021 Dmitry Maslov
+// For conditions of distribution and use, see readme.txt
+
+#pragma once
+
+#include "platform.h"
+#include "singleton.h"
+
+#include <string>
+#include <vector>
+
+namespace util {
+
+//--------------------------------------------------------------------------------------------------------------------------------
+class SystemInfo : public Singleton<SystemInfo>
+{
+public:
+	struct CoreCount {
+		unsigned logical;	// Кол-во логических процессоров (макс. число одновременно исполняемых потоков)
+		unsigned physical;	// Кол-во физических процессоров (равно logical, если CPU без технологии HT)
+	};
+
+	// Возвращает ссылку на структуру с информацией о количестве
+	// логических и физических процессоров (ядер CPU) в системе
+	const CoreCount& GetCoreCount() const { return m_CoreCount; }
+	// Возвращает дату и время запуска (UTC) в формате DateTime. Инициализируется
+	// значением текущего системного времени ОС в момент инициализации SystemInfo
+	uint64_t GetLaunchDateTime() const { return m_LaunchDateTime; }
+
+	// Возвращает список параметров командной строки (параметры запуска приложения)
+	const std::vector<std::wstring>& GetCommandLineParameters() const { return m_CmdLineParameters; }
+
+	// Возвращает полный путь к исполнимому файлу приложения
+	const std::wstring& GetAppExePath() const { return m_AppExePath; }
+	// Возвращает полный путь (оканчивающийся слешем) к директории локальных пользовательских данных приложений
+	const std::wstring& GetAppDataPath() const { return m_AppDataPath; }
+
+	// Возвращает количество секунд, прошедших с момента запуска приложения, а точнее с момента инициализации SystemInfo.
+	// На Windows Server 2003 и Windows XP значение ограничено 4,294,967 секундами (примерно 49.71 суток). При превышении
+	// этого порога отсчёт снова начинается с 0. На более новых ОС семейства Windows такого ограничения нет
+	unsigned GetAppUptime() const;
+
+	// Возвращает количество секунд, прошедших с момента старта операционной системы. На Windows Server 2003
+	// и Windows XP эта функция возвращает корректное значение, если на момент инициализации SystemInfo время,
+	// прошедшее с момента старта ОС, не превышало 49.71 суток, и только до момента, пока время работы самого
+	// приложения не превысит это значение. На более новых ОС семейства Windows такого ограничения нет
+	static unsigned GetOSUptime();
+
+	// Возвращает символ, используемый функциями форматирования как разделитель целой и дробной части чисел с плавающей
+	// запятой. Если параметр localeChanged равен false, то функция вернёт значение из кеша, а если он равен true, то
+	// обновит кеш и вернёт актуальное значение. Если локаль программы не устанавливалась, то это всегда будет '.'
+	char GetDecimalPoint(bool localeChanged = false) const;
+
+	// Возвращает true, если приложение консольное (точнее, если к нему прикреплено окно
+	// консоли), и возвращает false, если это обычное приложение (без окна консоли)
+	static bool IsConsoleApp();
+
+protected:
+	SystemInfo();
+	virtual ~SystemInfo() override = default;
+
+	void InitCoreCount();
+	static bool GetNextCmdLineParam(std::wstring& out, const wchar_t*& cmdLine);
+
+private:
+	CoreCount m_CoreCount = { 1, 1 };
+	std::vector<std::wstring> m_CmdLineParameters;
+
+	std::wstring m_AppExePath;
+	std::wstring m_AppDataPath;
+
+	uint64_t m_LaunchDateTime = 0;
+	unsigned m_FirstTick = 0;
+};
+
+} // namespace util
