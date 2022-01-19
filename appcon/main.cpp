9f65434062a6361f2f70bfa8200ad1b6b5bc0072
Dmitry Maslov <dmaslov81@gmail.com>
2022-01-19 17:38:39 +1000

    More code for console application sample

--- appcon/main.cpp
+++ appcon/main.cpp
@@ -3,14 +3,43 @@
 // For conditions of distribution and use, see readme.txt
 
 #include <auxlib/print.h>
+#include <core/strutil.h>
 #include <core/util.h>
+
+#include <stdio.h>
+#include <string>
+#include <string.h>
+
+//--------------------------------------------------------------------------------------------------------------------------------
+static std::string GetBuildDateTime(const char* date, const char* time)
+{
+	int day = 1, month = 0, year = 2000;
+	if (date && strlen(date) > 4)
+	{
+		sscanf_s(date + 4, "%d %d", &day, &year);
+		const char* months = "JanFebMarAprMayJunJulAugSepOctNovDec";
+		for (; month < 11 && util::StrNInsCmp(date, &months[3 * month], 3); ++month);
+	}
+
+	char dateTime[28];
+	// NB: 28 байт - мин. размер, достаточный на случай некорректных входных данных
+	sprintf_s(dateTime, util::CountOf(dateTime), "%4d-%02d-%02d ", year, month + 1, day);
+
+	if (time && *time)
+	{
+		strncpy_s(dateTime + 11, util::CountOf(dateTime) - 11, time, 5);
+	}
+
+	return std::string(dateTime, (time && *time) ? 16 : 10);
+}
 
 //--------------------------------------------------------------------------------------------------------------------------------
 int wmain(int argC, const wchar_t* argA[], const wchar_t* envA[])
 {
 	util::CheckMinimalRequirements();
 
-	aux::Printc("#3AML project. #7Console application sample.\n");
+	std::string buildVer = GetBuildDateTime(__DATE__, __TIME__);
+	aux::Printf("#3AML project. #7Console application sample. #8Built on %s\n", buildVer.c_str());
 
 	return 0;
 }
